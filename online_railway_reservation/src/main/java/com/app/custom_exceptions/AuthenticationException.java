e51f8c8d839497b635c8697611c507beae4c795c
KD4_Avdhut_83603 <158133549+KD4-Avdhut-83603@users.noreply.github.com>
2024-08-15 07:23:41 +0530

    value

--- online_railway_reservation/src/main/java/com/app/custom_exceptions/AuthenticationException.java
+++
@@ -1,9 +0,0 @@
-package com.app.custom_exceptions;
-
-@SuppressWarnings("serial")
-public class AuthenticationException extends RuntimeException {
-	public AuthenticationException(String message) {
-		super(message);
-	}
-
-}
